ccd3443cd275f3fcfc38014b59fb7dc098b455bd
Brecht Maes <bmaes@edu.lapinamk.fi>
2026-05-04 12:52:55 +0300

    BUP 3

--- vite.config.ts
+++ vite.config.ts
@@ -4,27 +4,30 @@ import { defineConfig } from 'vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [
-		tailwindcss(),
-		sveltekit(),
-		SvelteKitPWA({
-			srcDir: './src',
-			mode: 'development',
-			strategies: 'generateSW',
-			registerType: 'autoUpdate',
-			manifest: {
-				name: 'Brecht Maes Portfolio',
-				short_name: 'Brecht',
-				theme_color: '#000000',
-				background_color: '#000000',
-				display: 'standalone'
-			}
-		})
-	],
-	server: {
-		headers: {
-			'Cross-Origin-Embedder-Policy': 'require-corp',
-			'Cross-Origin-Opener-Policy': 'same-origin'
-		}
-	}
+    plugins: [
+        tailwindcss(),
+        sveltekit(),
+        SvelteKitPWA({
+            srcDir: './src',
+            mode: 'development',
+            strategies: 'generateSW',
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Brecht Maes Portfolio',
+                short_name: 'Brecht',
+                theme_color: '#000000',
+                background_color: '#000000',
+                display: 'standalone'
+            }, // <--- Hier ontbrak de komma!
+            workbox: {
+                maximumFileSizeToCacheInBytes: 10485760, 
+            }
+        })
+    ],
+    server: {
+        headers: {
+            'Cross-Origin-Embedder-Policy': 'require-corp',
+            'Cross-Origin-Opener-Policy': 'same-origin'
+        }
+    }
 });
